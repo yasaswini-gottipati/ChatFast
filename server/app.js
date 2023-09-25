a87a5fc72b09d931223e839679e0b9a4a87fc435
yasaswini <yasaswini1502@gmail.com>
2023-09-25 19:54:27 +0530

    cors addition

--- server/app.js
+++ server/app.js
@@ -15,7 +15,11 @@ mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true, useUnifiedTopolo
 
 const app=express();
 const port=process.env.PORT;
-app.use(cors());
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth",userRoutes);
