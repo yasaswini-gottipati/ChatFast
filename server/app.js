692c378ba93bfc15ea112056287429356d9aff8b
Yasaswini Gottipati <yasaswini1502@gmail.com>
2023-09-30 15:13:14 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,14 +16,14 @@ mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true, useUnifiedTopolo
 const app=express();
 const port=process.env.PORT;
 app.use(cors({
-    origin:"https://chat-fast-frontend.vercel.app",
+    origin:"https://chat-fast-frontend.vercel.app/login",
     methods:["POST","GET"],
     credentials: true
 }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://chat-fast-frontend.vercel.app'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Origin', 'https://chat-fast-frontend.vercel.app/login'); // Allow requests from any origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
